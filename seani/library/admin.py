446385ffb5448523027b11d33434ce8027711682
SantiagoHM27 <himejimadarkrai@gmail.com>
2024-02-16 15:11:38 -0600

    01 App Library Module and Question Models

---
+++ seani/library/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from.models import  Question, Module
+from .models import Question, Module
+
+# Register your models here.
+admin.site.register(Module)
+admin.site.register(Question)
